407ab5ce6b5821d7bb835f265b45f9db6a40c74f
bggarcia <48252302+bggarcia@users.noreply.github.com>
2023-05-23 23:41:02 +0200

    Update Figura.h

--- Figura.h
+++ Figura.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Vector2D.h"
+#include "Tablero.h"
 #define NUM 16
 
 class Figura
@@ -8,10 +9,12 @@ protected:
     //float altura;
     Vector2D posicion;
     int ident;
+    Tablero* posiciones Legales[NUM];
 public:
     Figura();
     virtual ~Figura();
-   
+    friend class Lista Tableros;
+    friend class Tablero;
     Vector2D getPos();
     void setPos(float x, float y);
     void setPos(Vector2D pos);
