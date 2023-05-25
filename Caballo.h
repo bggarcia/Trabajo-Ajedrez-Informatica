cd02ebba56a2a05412e35e43eb4900ce8e885ee9
robervm <111271591+robervm@users.noreply.github.com>
2023-05-25 23:30:25 +0200

    Update Caballo.h

--- Caballo.h
+++ Caballo.h
@@ -1,6 +1,15 @@
 #pragma once
-class Caballo : public Pieza {
-  
-  
+#include "Figura.h"
+class Caballo : public Figura {
+private:
+	float lado;
+	Tablero* posicionesLegales[NUM];
+	Vector2D max, min;
+public:
+	Caballo(bool color_) :Figura(color, 'c') { lado = 0.1f; }
+	Caballo();
+	void dibuja();
+	bool movLegal(int ident_);
 };
+
 
