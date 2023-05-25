5a261b23b0f374c28dcf3bc122f283012308dd5f
robervm <111271591+robervm@users.noreply.github.com>
2023-05-25 17:50:59 +0200

    Update Alfil.h

--- Alfil.h
+++ Alfil.h
@@ -1,5 +1,13 @@
 #pragma once
 class Alfil : public Pieza {
-  
+  private:
+	float lado;
+	Tablero* posicionesLegales[NUM];
+	Vector2D max, min;
+public:
+	Alfil(bool color_) :Figura(color, 'a') { lado = 0.1f; }
+	Alfil();
+	void dibuja();
+	bool movLegal(int ident_);
 };
 
