f75f534efabeba90a728c74577279d6964a8a740
amayore <47856800+amayore@users.noreply.github.com>
2023-05-26 23:47:44 +0200

    Update Coordinador.h

--- Coordinador.h
+++ Coordinador.h
@@ -1,8 +1,13 @@
-#pragmaonce
+#pragma once
 #include "Mundo.h
 
 class Coordinador
 {
   public: 
+    Coordinador();
+	  virtual ~Coordinador();
   protected: 
+    Mundo mundo;
+  	enum Estado { INICIO, JUEGO, GAMEOVER, FIN, PAUSE };
+
 }
