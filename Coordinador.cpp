a9666bf5c26c8e382d4e079078ce906e2c7052e6
bggarcia <48252302+bggarcia@users.noreply.github.com>
2023-05-26 23:34:55 +0200

    Update Coordinador.cpp

--- Coordinador.cpp
+++ Coordinador.cpp
@@ -5,7 +5,15 @@ void Coordinador::tecla(unsigned char key)
 {
   if (estado==INICIO)
   {
-    
+    ETSIDI::play("bin/sonidos/audio.mp3");
+        ETSIDI::playMusica("bin/sonidos/CancionDeFondo.mp3", 1);
+        if (key == 'e')
+        {
+            mundo.inicializa();
+            estado = JUEGO;
+        }
+        if (key == 's')
+            exit(0);
   }
   else if (estado==JUEGO)
   {
