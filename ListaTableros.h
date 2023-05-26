af2345c11a594ac26ad4447ab501f4935700f90e
robervm <111271591+robervm@users.noreply.github.com>
2023-05-26 23:13:58 +0200

    #define y atributos de ListaTableros.h

--- ListaTableros.h
+++ ListaTableros.h
@@ -3,22 +3,15 @@
 #include "Figura.h"
 #define MAX_TABLEROS 8
 #define NUM 14
+#define CAS 64
 class ListaTableros
 {
 private:
     Tablero **listaa;
-    Tablero* posicionesLegales[NUM]; //nuestra matriz
-        
+    Tablero* posicionesLegales[NUM];
+    Tablero* casillasOcupadas[CAS];
 public:
     ListaTableros();
     virtual ~ListaTableros();
-    void Dibuja();
-    void setPos();
-    Vector2D getMin();
-    Vector2D getMax();
-    void setIndent();
-    void movement(Figura& f);
-    //friend class Interaccion; //mas adelante
-    friend class Figura;
+   
 };
-
