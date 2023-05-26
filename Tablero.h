54e6bfe597741e7e8c33a2148f669c3b078cfaa8
robervm <111271591+robervm@users.noreply.github.com>
2023-05-26 23:03:35 +0200

    Update Tablero.h

--- Tablero.h
+++ Tablero.h
@@ -1,31 +1,19 @@
 #pragma once
-#include "Casilla.h"
-#include <iostream>
 #include "Vector2D.h"
 #include "ColorRGB.h"
-
 class Tablero
 {
 private:
-	Vector2D limite1;
-	Vector2D limite2;
-	ColorRGB color;
-	Vector2D pos;
-	int ident;
+    
+    Vector2D limite1;
+    Vector2D limite2;
+    ColorRGB color;
+    Vector2D pos;
+    int ident;
 public:
-	//Tablero(int identi,float x1,float y1);
-	Tablero(float x1,float y1);
-	virtual ~Tablero();
-	void dibuja();
-	void SetColor(Byte r, Byte v, Byte a);
-	void SetPos(float x1, float y1, float x2, float y2);
-	void identi(int i,int j);
-    	int getIdent();
-	
-	Vector2D getPos();
-    	Vector2D getPos1();
-    	float getPosX();
-    	float getPosY();
-    	//friend class Interaccion; //mas adelante
+    Tablero();
+
+   
 };
+
 
