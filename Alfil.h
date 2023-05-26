20215d743849c1680ed82f6bcfc4c6ecfca8fd70
jhuirse <80428998+jhuirse@users.noreply.github.com>
2023-05-26 23:06:22 +0200

    Nueva estructura de clase Alfil

--- Alfil.h
+++ Alfil.h
@@ -1,13 +1,17 @@
 #pragma once
-class Alfil : public Pieza {
+class Alfil : public Figura {
   private:
 	float lado;
 	Tablero* posicionesLegales[NUM];
 	Vector2D max, min;
 public:
-	Alfil(bool color_) :Figura(color, 'a') { lado = 0.1f; }
+	Alfil(bool color_, const std::string& tipo_) :Figura(color, tipo) {
+		lado = 0.1f;
+		color=color_;
+	}
 	Alfil();
 	void dibuja();
 	bool movLegal(int ident_);
+	std::string getName();
 };
 
