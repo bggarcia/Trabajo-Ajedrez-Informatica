77cb24557ae3eea8a04fd6274f6ed11a5eb146ac
jhuirse <80428998+jhuirse@users.noreply.github.com>
2023-05-26 23:16:21 +0200

    Reestructuración de clase Figura
    Añadidas funciones movLegal, getName, setPos(int ident)

--- Figura.h
+++ Figura.h
@@ -19,14 +19,21 @@ protected:
     SpriteSequence sprite2{ "bin/imagenes/piezas_negras.png", 6 };
 public:
     Figura();
+    Figura(bool color_, const std::string& tipo_);
     virtual ~Figura();
-    friend class Lista Tableros;
-    friend class Tablero;
+    virtual void dibuja()=0;
+    virtual bool movLegal (int ident_)=0;
+    virtual std::string getName()=0;
+
     Vector2D getPos();
     void setPos(float x, float y);
     void setPos(Vector2D pos);
+    void setPos(int ident);
     //
     void setIdent(int identi);
     int getIdent();
 
+    
+    friend class Lista Tableros;
+    friend class Tablero;
 };
