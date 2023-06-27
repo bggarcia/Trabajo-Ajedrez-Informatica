a4317010ac6b521e95a66f98874c765d587aa4d4
amayore <47856800+amayore@users.noreply.github.com>
2023-06-27 11:24:24 +0200

    Constructores Rey.h

--- Rey.h
+++ Rey.h
@@ -3,5 +3,16 @@ class Rey : public Pieza {
 private:
 	float lado;
 	bool primerMov;
+public:
+	Rey(bool color_, const std::string& tipo_, int identidad_, bool vida_) :Figura(color_, tipo_, identidad_, vida_) {
+		lado = 0.1f;
+	}
+	Rey(bool color_, int identidad_) :Figura(color_, identidad_) {
+		lado = 0.1f;
+		primerMov = true;
+		tipo = "k";
+		
+	}
+	Rey();
 };
 
