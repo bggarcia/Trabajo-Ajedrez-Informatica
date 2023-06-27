5c638885879d77d4c31357013a872de71843fb95
amayore <47856800+amayore@users.noreply.github.com>
2023-06-27 11:31:47 +0200

    constructores Torre.h

--- Torre.h
+++ Torre.h
@@ -4,7 +4,14 @@ class Torre : public Pieza {
 private:
 	float lado;
 public:
-	
+	Torre(bool color_, const std::string& tipo_,int identidad_,bool vida_) :Figura(color_, tipo_,identidad_,vida_) {
+		lado = 0.1f;
+	}
+	Torre(bool color_, int identidad_) :Figura(color_, identidad_) {
+		lado = 0.1f;
+		tipo = "t";	
+	}
+	Torre();
 	void dibuja();
 	void movLegal(int ident_);
 	std::string getName();
