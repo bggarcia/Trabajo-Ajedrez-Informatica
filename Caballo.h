242b165ee1d9e2851f38028a2648fd43bac60761
jhuirse <80428998+jhuirse@users.noreply.github.com>
2023-06-28 02:10:34 +0200

    Modificación Caballo.h : Nuevo constructor
    El nuevo constructor hace un set del color, identidad y tipo

--- Caballo.h
+++ Caballo.h
@@ -2,11 +2,11 @@
 #include "Figura.h"
 class Caballo : public Figura {
 private:
-	float lado;
+	float lado=0.1f;
 	Tablero* posicionesLegales[NUM];
 	Vector2D max, min;
 public:
-	Caballo(bool color_) :Figura(color, 'c') { lado = 0.1f; }
+	Caballo(bool color_,int identidad_) :Figura(color_,identidad_)                {tipo="c";
 	Caballo();
 	void dibuja();
 	bool movLegal(int ident_);
