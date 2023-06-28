a8a979fde80b617f476011a73a1b434060b729bc
jhuirse <80428998+jhuirse@users.noreply.github.com>
2023-06-28 02:02:16 +0200

    Código de obtención de la posición inicial y final

--- Caballo.cpp
+++ Caballo.cpp
@@ -18,6 +18,16 @@ void Caballo::dibuja() {
 
 bool Caballo::movLegal(int ident_) {
    Vector2D pos_ini,pos,dif,dif_aux; //Posición onicial, posición final, diferencia de posiciones y valores absolutos de la diferencia.
+	int aux_x,aux_y;
+	pos_ini=this->getPos();
+
+	//Se procede a la separación de la identidad en variables auxiliares para que la división sea 
+	//una división de enteros, pero que luego se pase como float al vector de posición.
+	
+	aux_x=ident/10;
+	aux_y=ident %10;
+	pos= {(float)aux_x,(float)aux_y};
+
 	
 
 }
