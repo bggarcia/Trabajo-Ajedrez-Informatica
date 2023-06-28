72e0ebe57a4501ec29ab2166955038046a4fd6f4
bggarcia <48252302+bggarcia@users.noreply.github.com>
2023-06-28 16:22:18 +0200

    Update Reina.h
    Atributos de Reina y constructor

--- Reina.h
+++ Reina.h
@@ -1,4 +1,10 @@
 #pragma once
 class Reina : public Pieza {
+private:
+  float lado;
+public:
+  Reina(bool color_,const std::string& tipo_,int identidad_,bool vida_):Figura(clor_,tipo_,identidad_,vida_){
+    lado=0.1f;
+  }
 };
-
+	
