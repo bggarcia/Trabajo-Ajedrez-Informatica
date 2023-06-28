9304d449c617d115aa4baed3bd439bb59b1b0cdb
jhuirse <80428998+jhuirse@users.noreply.github.com>
2023-06-28 12:42:53 +0200

    Eliminación de variables sin usar y solución de error

--- Caballo.h
+++ Caballo.h
@@ -3,10 +3,9 @@
 class Caballo : public Figura {
 private:
 	float lado=0.1f;
-	Tablero* posicionesLegales[NUM];
-	Vector2D max, min;
+
 public:
-	Caballo(bool color_,int identidad_) :Figura(color_,identidad_)                {tipo="c";
+	Caballo(bool color_,int identidad_) :Figura(color_,identidad_)                {tipo="c";}
 	Caballo();
 	void dibuja();
 	bool movLegal(int ident_);
