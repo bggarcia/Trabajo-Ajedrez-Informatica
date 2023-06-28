83ea94f76a43d53680a87ea042cd3553cffa1d6b
jhuirse <80428998+jhuirse@users.noreply.github.com>
2023-06-28 21:42:10 +0200

    Función dibuja de Caballo

--- Caballo.cpp
+++ Caballo.cpp
@@ -7,6 +7,26 @@ Caballo::Caballo(){
 }
 
 void Caballo::dibuja() {
+	if (color==1){
+		 sprite1.setCenter(0.0f, 0.0f);
+        sprite1.setSize(0.8f, 0.8f);
+        glPushMatrix();
+        glTranslatef(posicion.x - 0.25f, posicion.y - 0.25f, 0.2f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        sprite1.setState(1);
+        sprite1.draw();
+        glPopMatrix();
+    }
+    else {
+        sprite2.setCenter(0.0f, 0.0f);
+        sprite2.setSize(0.8f, 0.8f);
+        glPushMatrix();
+        glTranslatef(posicion.x - 0.25f, posicion.y - 0.25f, 0.2f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        sprite2.setState(1);
+        sprite2.draw();
+        glPopMatrix();
+    }
 
 }
 
