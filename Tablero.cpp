922414c18636e9655ae49cf032f1c3cbd67157da
robervm <111271591+robervm@users.noreply.github.com>
2023-06-28 23:05:33 +0200

    Funcion dibuja, junto con la libreria estidi

--- Tablero.cpp
+++ Tablero.cpp
@@ -20,3 +20,64 @@ void Tablero::setColor(Byte r, Byte v, Byte a)
 {
     color.set(r, v, a);
 }
+void Tablero::dibuja()
+{
+    //FUENTES
+    ETSIDI::setTextColor(233, 190, 107);
+    ETSIDI::setFont("bin/fuentes/Classica-BoldOblique.ttf", 28);
+    ETSIDI::printxy("  A", 0, -1); ETSIDI::printxy("  1", -1, 1);
+    ETSIDI::printxy("   B", 1, -1); ETSIDI::printxy("  2", -1, 2);
+    ETSIDI::printxy("   C", 2, -1); ETSIDI::printxy("  3", -1, 3);
+    ETSIDI::printxy("   D", 3, -1); ETSIDI::printxy("  4", -1, 4);
+    ETSIDI::printxy("   E", 4, -1); ETSIDI::printxy("  5", -1, 5);
+    ETSIDI::printxy("   F", 5, -1); ETSIDI::printxy("  6", -1, 6);
+    ETSIDI::printxy("   G", 6, -1); ETSIDI::printxy("  7", -1, 7);
+    ETSIDI::printxy("   H", 7, -1); ETSIDI::printxy("  8", -1, 8);
+
+    //TABLERO
+    glDisable(GL_LIGHTING);
+    color.ponColor();
+    glBegin(GL_POLYGON);
+    glVertex3d(limite1.x, limite1.y, -0.04f);
+    glVertex3d(limite2.x, limite1.y, -0.04f);
+    glVertex3d(limite2.x, limite2.y, -0.04f);
+    glVertex3d(limite1.x, limite2.y, -0.04f);
+    glEnd();
+    glEnable(GL_LIGHTING);
+
+    //MARCO TABLERO
+
+    glBegin(GL_POLYGON);
+    glColor3ub(98, 24, 8);
+    glVertex3f(limite1.x - 0.1f, limite1.y - 0.1f, -0.05f);
+    glVertex3f(limite2.x + 0.1f, limite1.y - 0.1f, -0.05f);
+    glVertex3f(limite2.x + 0.1f, limite2.y + 0.1f, -0.05f);
+    glVertex3f(limite1.x - 0.1f, limite2.y + 0.1f, -0.05f);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3ub(233, 190, 107);
+    glVertex3f(limite1.x - 0.2f, limite1.y - 0.2f, -0.06f);
+    glVertex3f(limite2.x + 0.2f, limite1.y - 0.2f, -0.06f);
+    glVertex3f(limite2.x + 0.2f, limite2.y + 0.2f, -0.06f);
+    glVertex3f(limite1.x - 0.2f, limite2.y + 0.2f, -0.06f);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3ub(98, 24, 8);
+    glVertex3f(limite1.x - 0.3f, limite1.y - 0.3f, -0.07f);
+    glVertex3f(limite2.x + 0.3f, limite1.y - 0.3f, -0.07f);
+    glVertex3f(limite2.x + 0.3f, limite2.y + 0.3f, -0.07f);
+    glVertex3f(limite1.x - 0.3f, limite2.y + 0.3f, -0.07f);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glColor3ub(233, 190, 107);
+    glVertex3f(limite1.x - 0.4f, limite1.y - 0.4f, -0.08f);
+    glVertex3f(limite2.x + 0.4f, limite1.y - 0.4f, -0.08f);
+    glVertex3f(limite2.x + 0.4f, limite2.y + 0.4f, -0.08f);
+    glVertex3f(limite1.x - 0.4f, limite2.y + 0.4f, -0.08f);
+    glEnd();
+
+
+}
