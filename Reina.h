65f09deacb92c61a10a72b18eed1c9223b35e1e7
bggarcia <48252302+bggarcia@users.noreply.github.com>
2023-06-28 16:24:04 +0200

    Update Reina.h
    Segundo constructor de Reina con diferentes parametros

--- Reina.h
+++ Reina.h
@@ -6,5 +6,9 @@ public:
   Reina(bool color_,const std::string& tipo_,int identidad_,bool vida_):Figura(clor_,tipo_,identidad_,vida_){
     lado=0.1f;
   }
+  Reina(bool color_,int identidad_):Figura(color_,identidad_){
+    lado=0.1f;
+    tipo="q";
+  }  
 };
 	
