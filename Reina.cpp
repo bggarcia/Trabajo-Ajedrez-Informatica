ef5182d2c9ba04c97701dcffad530c4c4203007e
bggarcia <48252302+bggarcia@users.noreply.github.com>
2023-06-28 23:06:53 +0200

    Update Reina.cpp
    funcion dibuja de reina

--- Reina.cpp
+++ Reina.cpp
@@ -1,1 +1,31 @@
 #include "Reina.h"
+
+
+Reina::Reina(){
+  lado=0.1f;
+}
+
+void Reina::dibuja(){
+  if (color==1)
+  {
+    sprite1.setCenter(0.0f, 0.0f);
+        sprite1.setSize(0.8f, 0.8f);
+        glPushMatrix();
+        glTranslatef(posicion.x - 0.25f, posicion.y - 0.25f, 0.2f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        sprite1.setState(4);
+        sprite1.draw();
+        glPopMatrix();
+  }
+  else
+  {
+        sprite2.setCenter(0.0f, 0.0f);
+        sprite2.setSize(0.8f, 0.8f);
+        glPushMatrix();
+        glTranslatef(posicion.x - 0.25f, posicion.y - 0.25f, 0.2f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        sprite2.setState(4);
+        sprite2.draw();
+        glPopMatrix();
+  }
+}
