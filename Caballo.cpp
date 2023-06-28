0d6fdf62f8499c51179acff422984427c134cd7d
jhuirse <80428998+jhuirse@users.noreply.github.com>
2023-06-28 19:45:23 +0200

    Eliminado anterior código de Caballo::dibuja()

--- Caballo.cpp
+++ Caballo.cpp
@@ -7,13 +7,7 @@ Caballo::Caballo(){
 }
 
 void Caballo::dibuja() {
-	
 
-	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y, 0);
-	glColor3f(0.0f, 0.0f, 0.0f);
-	glutSolidSphere(lado, 20, 20);
-	glPopMatrix();
 }
 
 bool Caballo::movLegal(int ident_) {
