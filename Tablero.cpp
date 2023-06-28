4883ba48cf12b7487c2cf806b72b96163b029e31
robervm <111271591+robervm@users.noreply.github.com>
2023-06-28 22:58:42 +0200

    Constructor, destructor y librerías de tablero

--- Tablero.cpp
+++ Tablero.cpp
@@ -5,11 +5,7 @@ Tablero::Tablero(){
 //rojo = verde = azul = 255; //blanco
 }
 
-/*Tablero::Tablero(int identi, float x1, float y1) {
-    ident = identi;
-    this->pos.x =x1;
-    this->pos.y = y1;
-}*/
+
 
 Tablero::Tablero(float x1,float y1){
        this->pos.x=x1;
@@ -18,53 +14,6 @@ Tablero::Tablero(float x1,float y1){
 Tablero::~Tablero()
 {
 	
-}
-	
-	
-	
-void Tablero::SetColor()
-{
-	
-	for (int i=0;i<8;i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			if ((i + j)%2 == 0)
-			{
-				casilla[i][j].SetColor(255, 255, 255);
-			}
-			else
-			{
-				casilla[i][j].SetColor(255, 0, 0);
-			}
-		}
-	}
-}
-
-void Tablero::dibuja()
-{
-    glDisable(GL_LIGHTING);
-    color.ponColor();
-    glBegin(GL_POLYGON);
-    glVertex3d(limite1.x, limite1.y,0);
-    glVertex3d(limite2.x, limite1.y,0);
-    glVertex3d(limite2.x, limite2.y,0);
-    glVertex3d(limite1.x, limite2.y,0);
-    glEnd();
-    glEnable(GL_LIGHTING);
-}
-
-void Tablero::SetPos()
-{
-	int i = 0, j = 0;
-	for ( i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			casilla[i][j].SetPos(j, i);
-	
-		}
-	}
 }
 
 
