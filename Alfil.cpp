e5145943df3bdb41c2c8ca8afa376e37779694fb
jhuirse <80428998+jhuirse@users.noreply.github.com>
2023-06-28 12:44:58 +0200

    Eliminación de warnings de conversión

--- Alfil.cpp
+++ Alfil.cpp
@@ -6,20 +6,20 @@ Alfil::Alfil() {
 
 void Alfil::dibuja() {
 	if (color == 1) {
-        sprite1.setCenter(0, 0);
-        sprite1.setSize(0.8, 0.8);
+        sprite1.setCenter(0.0f, 0.0f);
+        sprite1.setSize(0.8f, 0.8f);
         glPushMatrix();
-        glTranslatef(posicion.x - 0.25, posicion.y - 0.25, 0.2);
+        glTranslatef(posicion.x - 0.25f, posicion.y - 0.25f, 0.2f);
         glColor3f(1.0f, 0.0f, 0.0f);
         sprite1.setState(2);
         sprite1.draw();
         glPopMatrix();
     }
     else {
-        sprite2.setCenter(0, 0);
-        sprite2.setSize(0.8, 0.8);
+        sprite2.setCenter(0.0f, 0.0f);
+        sprite2.setSize(0.8f, 0.8f);
         glPushMatrix();
-        glTranslatef(posicion.x - 0.25, posicion.y - 0.25, 0.2);
+        glTranslatef(posicion.x - 0.25f, posicion.y - 0.25f, 0.2f);
         glColor3f(1.0f, 0.0f, 0.0f);
         sprite2.setState(2);
         sprite2.draw();
