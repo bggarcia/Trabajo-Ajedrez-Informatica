7da0c018474f92f7ad42172389176c6d81437633
jhuirse <80428998+jhuirse@users.noreply.github.com>
2023-06-28 11:19:50 +0200

    Eliminado código anterior de Mundo::dibuja

--- Mundo.cpp
+++ Mundo.cpp
@@ -8,29 +8,7 @@ void Mundo::dibuja()
             0.0, y_ojo, 0.0,
             0.0, 1.0, 0.0);
   
-  lt.dibuja(); //dibuja el tablero
-  //Peones blancos
-  p1b.dibuja();
-  p2b.dibuja();
-  p3b.dibuja();
-  p4b.dibuja();
-  p5b.dibuja();
-  p6b.dibuja();
-  p7b.dibuja();
-  p8b.dibuja();
-  //torres
-  t1b.dibuja();
-  t2b.dibuja();
-  //caballos
-  cb1.dibuja();
-  cb2.dibuja();
-  //alfiles
-  ab1.dibuja();
-  ab2.dibuja();
-  //reina
-  qb.dibuja();
-  //rey
-  kb.dibuja();
+ 
  }
 
 void Mundo::inicializa()
