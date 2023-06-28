877625707d38dce233c2d7d5eaab3319341e4d6d
jhuirse <80428998+jhuirse@users.noreply.github.com>
2023-06-28 10:59:43 +0200

    Mundo.h : atributos e inclusiones actualizadas

--- Mundo.h
+++ Mundo.h
@@ -1,7 +1,16 @@
 #pragma once
+#include "Tablero.h"
+#include "ETSIDI.h"
+#define NUM_FIGURAS 32
 class Mundo
 {
 public:
+virtual ~Mundo();
+Tablero lt;
+int ident,pos;
+bool turno;
+
+
 	float x_ojo;
 	float y_ojo;
 	float z_ojo;
